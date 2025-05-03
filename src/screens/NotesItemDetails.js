4ddee9f07f7e92bbf14bdc596a7229357867f8e2
utopicUnicorn <christianbarrios995@gmail.com>
2025-05-03 11:11:42 +0800

    added some basic Icon and styles for the notes

--- src/screens/NotesItemDetails.js
+++ src/screens/NotesItemDetails.js
@@ -1,18 +1,23 @@
+import { useState } from 'react';
 import {View, Text, ScrollView} from 'react-native';
 import AppLayout from '../layout/AppLayout';
 import {convertDate} from '../utils/utilsFunc';
 import GlobalStyles from '../styles/GlobalStyles.';
-import FAIcon from 'react-native-vector-icons'
+import FAIcon from 'react-native-vector-icons';
 
 export default function NotesItemDetails({route}) {
   const details = route.params.item;
+   const [editDetails, setEditDetails] = useState(false);
 
   console.log('eye tem', details);
   return (
     <AppLayout title={details.title} canBack>
       <ScrollView>
         <View>
-          <Text style={GlobalStyles.textPrimary}>Description:</Text>
+          <View>
+            <Text style={GlobalStyles.textPrimary}>Description:</Text>
+            <FAIcon onPress={handleEditDescription} name="floppy-o" size={22} />
+          </View>
           <Text style={GlobalStyles.textSecondary}>{details.description}</Text>
         </View>
 
